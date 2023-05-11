301c2102b887d453d40a8e1c805a48b52c44836a
ailana tariel <ailanatariel@gmail.com>
2023-05-11 19:34:08 +0300

    ScannerOdev

--- src/main/java/day04scannerwrapper/ScannerOdev.java
+++ src/main/java/day04scannerwrapper/ScannerOdev.java
@@ -1,14 +1,40 @@
 package day04scannerwrapper;
 
+import java.util.Scanner;
+
 public class ScannerOdev {
     public static void main(String[] args) {
         /*
-    Homework
-    1) Kullanicidan aldiginiz 3 tane sayinin ortalamasini bulan kodu yaziniz
-     ornegin => 3,5,7 icin ortalama (3+5+7)/3=5
-    2) Kullanicidan aldiginiz 3 basamakli bir sayinin rakamlari toplamini bulunuz
-     ornegin => 312 icin 3+1+2=6
+    1) Kullanicidan aldiginiz 3 tane sayinin ortalamasini bulan kodu yaziniz.
+    Ornegin ==> 3,5,7 icin ortalama (3+5+7)/3=5
+     */
 
- */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lutfen 3 sayi giriniz");
+
+        int sayi1 = input.nextInt();
+        int sayi2 = input.nextInt();
+        int sayi3 = input.nextInt();
+
+        int ortalama = (sayi1 + sayi2 + sayi3) / 3;
+        System.out.println("Girilen 3 sayinin ortalamasi = " + ortalama);
+
+        /*
+        2) Kullanicidan aldiginiz 3 basamakli bir sayinin rakamlari toplamini bulunuz
+        Ornegin ==> 312 icin rakamlari toplami 3+1+2=6
+         */
+
+        System.out.println("Lutfen 3 basamakli bir sayi giriniz");
+
+        int girilenSayi = input.nextInt();
+        int yuzlerBasamagi = girilenSayi%10;
+        girilenSayi = girilenSayi/10;
+        int onlarBasamagi = girilenSayi%10;
+        girilenSayi = girilenSayi/10;
+        int birlerBasamagi = girilenSayi%10;
+
+        System.out.println("Girilen sayinin rakamlari toplami = " + (yuzlerBasamagi+onlarBasamagi+birlerBasamagi));
+
+
     }
 }
