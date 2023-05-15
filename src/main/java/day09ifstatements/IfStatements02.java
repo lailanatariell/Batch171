9a1c26b3c53564f5177447ee1fab2d9799d46f6c
ailana tariel <ailanatariel@gmail.com>
2023-05-15 22:53:06 +0300

    if statements

--- src/main/java/day09ifstatements/IfStatements02.java
+++ src/main/java/day09ifstatements/IfStatements02.java
@@ -1,8 +1,28 @@
 package day09ifstatements;
 
+import java.util.Scanner;
+
 public class IfStatements02 {
     public static void main(String[] args) {
         //ornek: kullanicidan alinan tek mi cift mi oldugunu consala yazdiriniz
+        Scanner input=new Scanner(System.in);
+        System.out.println("lutfen bir sayi giriniz");
+        int sayi=input.nextInt();
+
+        if (sayi%2==0){
+            System.out.println("cift");
+
+        }
+        if(sayi%2!=0){
+            System.out.println("tek");
+        }
+        //........................//
+
+        if (sayi%2==0){
+            System.out.println("cift");
+        }else{
+            System.out.println("tek12");
+        }
 
 
     }
