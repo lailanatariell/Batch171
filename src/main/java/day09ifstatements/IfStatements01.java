edcf50c9e8cf3f06739cc107c9def945520031d7
ailana tariel <ailanatariel@gmail.com>
2023-05-15 22:40:07 +0300

    if statements

--- src/main/java/day09ifstatements/IfStatements01.java
+++ src/main/java/day09ifstatements/IfStatements01.java
@@ -4,6 +4,38 @@ public class IfStatements01 {
     public static void main(String[] args) {
         //ormek: sayiyi uc basamak ile ekrana "sayi uc basamaklidir "yazdirin
         int number=123;
+        number=Math.abs(number);
+        if (number>99 && number<1000){
+            System.out.println("sayi uc basamaklidir");
+
+        }
+
+        // intervew sorusu
+        // ornek: bir string deki tekrarsiz characterleri ekrana yazdiriniz
+
+        //aabbccdc -ad
+        String str="acacb";
+        char ch1=str.charAt(0);
+        if (str.indexOf(ch1)==str.lastIndexOf(ch1)){
+            System.out.println(ch1);
+        }
+        char ch2=str.charAt(1);
+        if (str.indexOf(ch2)==str.lastIndexOf(ch2)){
+            System.out.println(ch2);
+        }
+        char ch3=str.charAt(2);
+        if (str.indexOf(ch3)==str.lastIndexOf(ch3)){
+            System.out.println(ch3);
+        }
+        char ch4=str.charAt(3);
+        if (str.indexOf(ch4)==str.lastIndexOf(ch4)){
+            System.out.println(ch4);
+        }
+        char ch5=str.charAt(4);
+        if (str.indexOf(ch5)==str.lastIndexOf(ch5)){
+            System.out.println(ch5);
+        }
+
 
     }
 }
---
+++ src/main/java/day09ifstatements/IfStatements02.java
@@ -0,0 +1,9 @@
+package day09ifstatements;
+
+public class IfStatements02 {
+    public static void main(String[] args) {
+        //ornek: kullanicidan alinan tek mi cift mi oldugunu consala yazdiriniz
+
+
+    }
+}
