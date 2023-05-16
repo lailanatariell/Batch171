c8668e437847e21a61b510b22f86330fc2b4dbe6
ailana tariel <ailanatariel@gmail.com>
2023-05-16 20:47:15 +0300

    if statements

--- src/main/java/day10ifstatements/IfStatemens01.java
+++ src/main/java/day10ifstatements/IfStatemens01.java
@@ -1,7 +1,21 @@
 package day10ifstatements;
 
+import java.util.Scanner;
+
 public class IfStatemens01 {
     public static void main(String[] args) {
+        //ornek: verilen bir sayinin pozitif negatif  veya notr oldugunu kontrol eden kodu yaziniz
+        Scanner input= new Scanner(System.in);
+        System.out.println("lutfen bir sayiya giriniz");
+        int num = input.nextInt();
+
+        if (num > 0) {
+            System.out.println("pozitif");
+        }else if(num<0){
+            System.out.println("negatif");
+        }else{
+            System.out.println("notr");
+        }
 
     }
 }
---
+++ src/main/java/day10ifstatements/IfStatements02.java
@@ -0,0 +1,8 @@
+package day10ifstatements;
+
+public class IfStatements02 {
+    public static void main(String[] args) {
+        //ornek: Gun sayisini verdiginizde gun ismini yazan kodu yaziniz
+        //  1==> Pazar, 2 ==> Pazartesi .....
+    }
+}
