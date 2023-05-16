9c74fd147fad597b6c2373988bd246e8320713e6
ailana tariel <ailanatariel@gmail.com>
2023-05-16 21:00:49 +0300

    if statements

--- src/main/java/day10ifstatements/IfStatements02.java
+++ src/main/java/day10ifstatements/IfStatements02.java
@@ -1,8 +1,37 @@
 package day10ifstatements;
 
+import java.util.Scanner;
+
 public class IfStatements02 {
     public static void main(String[] args) {
         //ornek: Gun sayisini verdiginizde gun ismini yazan kodu yaziniz
         //  1==> Pazar, 2 ==> Pazartesi .....
+        Scanner input=new Scanner(System.in);
+        System.out.println("lutfen kacinci gun oldugunu giriniz?");
+        byte num=input.nextByte();
+
+        if(num==1){
+            System.out.println("pazar");
+        } else if (num==2) {
+            System.out.println("pazartesi");
+
+        } else if (num==3) {
+            System.out.println("sali");
+
+        } else if (num==4) {
+            System.out.println("carsamba");
+
+        } else if (num==5) {
+            System.out.println("persembe");
+
+        } else if (num==6) {
+            System.out.println("cuma");
+
+        } else if(num==7) {
+            System.out.println("sumartesi");
+
+        }else{
+            System.out.println("boyle bir gun yok");
+        }
     }
 }
