e40dd559b9d97efc6d500a2dced622639c7b46a3
ailana tariel <ailanatariel@gmail.com>
2023-05-23 21:19:30 +0300

    loops

--- src/main/java/day14loops/loops01.java
+++ src/main/java/day14loops/loops01.java
@@ -1,2 +1,39 @@
-package day14loops;public class loops01 {
+package day14loops;
+
+public class loops01 {
+    //ornek: bir string i ters ceviren kodu yaziniz
+    //java==> avaj
+    public static void main(String[] args) {
+        String t="java";
+        String ters="";
+        for (int i = t.length()-1; i <0 ; i--) {
+
+            ters=ters+ t.substring(i,i+1);//string return eder
+            // birincisi dahil,ikincisi haric
+
+        }
+        System.out.println(ters);
+        //2.yol
+
+        String u= "Java";
+        String ters2="";
+        for (int i =u.length()-1; i >=0 ; i--) {
+            ters2=ters2+u.charAt(i);//char return eder
+
+        }
+        System.out.println(ters2);
+
+        //ornek 2: size verilen bir string in polindrome olup olmadigini kontrol eden kodu yaziniz
+        // ey edip adanada pide ye, nalan, kucuk,ada
+        //logic: String i ters cevir sonra da duz hali ile ters halini karsilastir, ayni ise Polindrome dur.
+        String duz ="ey edip adanada pide ye";
+        String ters3="";
+
+        for (int i =duz.length()-1; i >=0 ; i--) {
+            ters3=ters3+duz.charAt(i);
+
+        }
+        System.out.println(ters3);
+
+    }
 }
