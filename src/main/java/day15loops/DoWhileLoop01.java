167f12ec496681a82512653cb53d21ceaf83bc61
ailana tariel <ailanatariel@gmail.com>
2023-05-24 22:07:34 +0300

    while

--- src/main/java/day15loops/DoWhileLoop01.java
+++ src/main/java/day15loops/DoWhileLoop01.java
@@ -22,5 +22,25 @@ public class DoWhileLoop01 {
            j++;
        }
        System.out.println();
+
+       //................................//
+       //Ornek 2 : 23 den 12 ye kadar cift sayilari console a yazdiriniz
+       int k =23;
+       while (k>11){
+           if (k%2==0){
+               System.out.println(k+" ");
+           }
+           k--;
+       }
+       //Ornek 3: 6 dan 19 a kadar tum tek sayilari console a yazdiriniz.
+       int a=6;
+
+       while(a<20){
+           if (a % 2 != 0) {
+               System.out.println(a + " ");
+
+           }
+           a++;
+       }
    }
 }
