b79857869a328f0fd8fee710930b6dbfec0ed5b8
ailana tariel <ailanatariel@gmail.com>
2023-05-24 21:10:05 +0300

    while

---
+++ src/main/java/day15loops/DoWhileLoop01.java
@@ -0,0 +1,26 @@
+package day15loops;
+
+public class DoWhileLoop01 {
+   /*
+     baslangiv degeri
+     while(loop calisma kurali){
+     calisacak kodlar
+
+     arttirma/azaltme
+     }
+
+    */
+   public static void main(String[] args) {
+       for (int i = 3; i < 7; i++) {
+           System.out.println(i+" ");
+
+       }
+
+       int j=3;
+       while (j<7){
+           System.out.print(j+" ");
+           j++;
+       }
+       System.out.println();
+   }
+}
