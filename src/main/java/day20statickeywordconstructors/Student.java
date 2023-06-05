07377e201170a5b66dd90e6db131b03ddc9c92b0
ailana tariel <ailanatariel@gmail.com>
2023-06-05 20:50:08 +0300

    constructors

---
+++ src/main/java/day20statickeywordconstructors/Student.java
@@ -0,0 +1,18 @@
+package day20statickeywordconstructors;
+
+public class Student {
+    /*
+    1)static variable(pasif ozellikler) veya static methodlar(aktif ozellikler)
+    tum objectler icin ortek elemandir.
+    2) static class memberlars uzerinde yapilan tum degisiklikler tum objectleri etkiliyr
+    3)static class memberlar Classa , non-static class memberlar objectlere monte edilir
+    mesela: bir class dan 100 tane object olusturdugunuzda non-static olanlar 100 kere olusturulur
+    ama static olanlar object sayisindan bagimsiz olarak bir kere olusturulurlar
+    4)static class memberlara ulasmak icin object olusturmaya gerek duyulmaz
+    ama non static class memberlara ulasmak icin object olusturmak sarttir
+    5) static variable larin diger adi "Class variable"dir
+    non static varibale larin diger adi"instance variable " veya "object variable"dir
+
+     */
+    String stdName="Tom Hanks";
+}
