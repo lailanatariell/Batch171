7fe2b859a99c6fa12293f905d0a50c517f735a0e
ailana tariel <ailanatariel@gmail.com>
2023-06-11 01:53:47 +0300

    array

--- src/main/java/day16arraysforeachloop/Arrays02.java
+++ src/main/java/day16arraysforeachloop/Arrays02.java
@@ -17,6 +17,29 @@ public class Arrays02 {
         System.out.println(Arrays.toString(arr));
 
         System.out.println(arr[0].length()+arr[arr.length-1].length());
+        //System.out.println(arr[0].length()+arr[4].length());
+        //arr.length-1 == arr[4]
+        //arr.length arr.nin uzunlugunu verir yani 5 , -1 deyince 5-1 dort olur bu yuzden arr[4] ile esdegerdir
+
+        // ornek: string bir array olusturun icine  5 tane eleman ekleyin, tum elemanlarin icerdigi karakter
+        //sayilari toplamini ekrana yazdirin
+
+        String brr[]=new String[5];
+        brr[0]="talas";
+        brr[1]="bishkek";
+        brr[2]="osh";
+        brr[3]="ohio";
+        brr[4]="naryn";
+
+        //1.yol for loop
+
+        int totalChar=0;
+        for (int i = 0; i < brr.length; i++) {
+            totalChar= totalChar+brr[i].length();
+        }
+        System.out.println(totalChar);
+
+
 
 
 
