44f0a277c386119a550be9a6f8acf65ac2827806
ailana tariel <ailanatariel@gmail.com>
2023-06-11 11:06:02 +0300

    array

---
+++ src/main/java/day17arrays/Arrays01.java
@@ -0,0 +1,17 @@
+package day17arrays;
+
+import java.util.Arrays;
+
+public class Arrays01 {
+
+
+    public static void main(String[] args) {
+        // arrayleri kisa yoldan nasil olusturabiliriz
+        int arr[]={63,7,313,8,4};
+        System.out.println(Arrays.toString(arr));
+
+        //ornek: veriler arrayde kac tane cift sayi kac tane tek sayi oldugunu bulan kodu yaziniz
+        int sayac=0;
+
+    }
+}
