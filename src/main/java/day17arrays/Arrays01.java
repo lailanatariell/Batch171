063f656030b65ca4871aac21730084aa4ce7f2e5
ailana tariel <ailanatariel@gmail.com>
2023-06-11 11:13:56 +0300

    array

--- src/main/java/day17arrays/Arrays01.java
+++ src/main/java/day17arrays/Arrays01.java
@@ -12,6 +12,13 @@ public class Arrays01 {
 
         //ornek: veriler arrayde kac tane cift sayi kac tane tek sayi oldugunu bulan kodu yaziniz
         int sayac=0;
+        for (int w:arr) {
+            if(w%2==0){
+                sayac++;
+            }
+        }
+        //System.out.println(arr.length-sayac);
+        System.out.println("arrayde "+sayac+ " adet cift sayi "+(arr.length-sayac)+ " adet tek sayi kullanilmistir");
 
     }
 }
