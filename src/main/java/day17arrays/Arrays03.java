28fab81d6f858f234d79fc83bbb47457eba4ce32
ailana tariel <ailanatariel@gmail.com>
2023-06-12 09:01:38 +0300

    array

---
+++ src/main/java/day17arrays/Arrays03.java
@@ -0,0 +1,10 @@
+package day17arrays;
+
+public class Arrays03 {
+    public static void main(String[] args) {
+        //specific bir elemanin array de olup olmadigini anlamak icin gereken kodu yaziniz
+
+        String name []= {"K","C"};
+
+    }
+}
