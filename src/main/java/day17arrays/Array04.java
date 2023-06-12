c7117a45335cc86620024bc8b583c809654f4a0e
ailana tariel <ailanatariel@gmail.com>
2023-06-12 14:05:53 +0300

    array

--- src/main/java/day17arrays/Array04.java
+++ src/main/java/day17arrays/Array04.java
@@ -11,5 +11,11 @@ public class Array04 {
         String words[]= s.split(" ");
         System.out.println(Arrays.toString(words));
         System.out.println(words.length);
+
+        //ornke: size verilen bir cumlede kac harf oldugunu gosteren kodu yazdiriniz
+
+        String letters[]= s.replaceAll("[^a-zA-Z]","").split("");
+        System.out.println(Arrays.toString(letters));
+        System.out.println(letters.length);
     }
 }
