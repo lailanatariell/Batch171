4cfecf44ee8223f05f30ca6f10589a97bfffa23b
ailana tariel <ailanatariel@gmail.com>
2023-06-12 09:09:06 +0300

    array

--- src/main/java/day17arrays/Arrays03.java
+++ src/main/java/day17arrays/Arrays03.java
@@ -4,7 +4,26 @@ public class Arrays03 {
     public static void main(String[] args) {
         //specific bir elemanin array de olup olmadigini anlamak icin gereken kodu yaziniz
 
-        String name []= {"K","C"};
+        String name []= {"K","C","R","A","S"};
+        String eleman ="R";
+
+        //1. yol
+        int counter=0;
+        for (String w: name){
+            if (w.equals(eleman)){
+                counter++;
+                break;
+            }
+        }
+        if (counter>0){
+            System.out.println("Array has "+ eleman);
+        }else {
+            System.out.println("Array does not have"+eleman);
+        }
+
+
+
+
 
     }
 }
