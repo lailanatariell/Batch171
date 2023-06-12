119a0f8b9e29f6540eec831396abd9caccb7cc41
ailana tariel <ailanatariel@gmail.com>
2023-06-12 22:15:06 +0300

    stringbuilder

--- src/main/java/day24stringbuilder/Sb02.java
+++ src/main/java/day24stringbuilder/Sb02.java
@@ -16,5 +16,31 @@ public class Sb02 {
 
         System.out.println(sb1.delete(0, 3));//baslagis indexi dahil bitis indexine kadar (haric) olan karakterleri siler
 
+        System.out.println(sb1.replace(2, 5, "o"));
+
+        //sb1.replace(2, 5, "o") methodu 2 dahil 5 haric yeni index 2,3,4 deki karakterlerin yerine "o" koyar
+
+        System.out.println(sb1.insert(3, "xxx"));
+
+        StringBuilder sb2=new StringBuilder("java ");
+        StringBuilder sb3=new StringBuilder("java ");
+
+        //sonuc 0 ise alfabetik olarak ayni siradalar demektir
+        //sonuc -1 ise sb2 sb3 ten alfabetik olarak bir onde demektir
+        //sonuc 1 ise sb2 sb3 ten alfabetik olarak bir sonra demektir
+
+
+        int r1=sb2.compareTo(sb3);
+        System.out.println(r1);
+
+        //stringbuilder nasil string e cevirilir
+
+        String str= sb2.toString().toUpperCase();
+        System.out.println(str);
+
+        StringBuilder newSb2 = new StringBuilder(str);
+        System.out.println(newSb2);
+
+
     }
 }
